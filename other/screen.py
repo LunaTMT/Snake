9d0be04bd68a5d24962f2050d3f179f3fd2a51fd
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-23 14:57:29 +0100

    Update screen.py

--- other/screen.py
+++ other/screen.py
@@ -13,9 +13,7 @@ class Screen:
         self.colour = colours.WHITE
         pygame.display.set_caption("Snake")
 
-    
-
-    def init_dimensions(self):
+    def init_dimensions(self) -> float:
         """
         Based upon the size of the board which is passed as an argument in main.py (k)
         The screen size and the game speed is altered accordingly for a relatively comfortable playing experience
@@ -23,23 +21,19 @@ class Screen:
         if self.k <= 5:
             self.cell_size = 50
             self.interface.game_speed = 500
-        
         elif self.k <= 10:
             self.cell_size = 40
             self.interface.game_speed = 200
-      
         elif self.k <= 20:
             self.cell_size = 30
             self.interface.game_speed = 100
-            
         elif self.k <= 40:
             self.cell_size = 25
             self.interface.game_speed = 100
-
         else:
             self.cell_size = 20
             self.interface.game_speed = 50
-
+    
         return self.cell_size * (self.k)
         
 
